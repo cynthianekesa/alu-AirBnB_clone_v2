d5851a85af7b5ffe073805e41edfd366087ce26c
Magotgarang <m.guut@alustudent.com>
2023-10-05 15:28:35 +0000

    updated

--- models/user.py
+++ models/user.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+<<<<<<< HEAD
+""" User Module for HBNB project """
+
+=======
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+>>>>>>> parent of 06df725 (models)
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+<<<<<<< HEAD
+    """ User class """
+    __tablename__ = 'users'
+
+=======
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+>>>>>>> parent of 06df725 (models)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+<<<<<<< HEAD
+    reviews = relationship("Review", backref="user", cascade="all, delete")
+=======
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
+>>>>>>> parent of 06df725 (models)
