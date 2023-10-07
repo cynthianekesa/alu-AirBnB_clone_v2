929c0d5bcd49d6e23dcde50527e9f55f9e7bab88
Magotgarang <m.guut@alustudent.com>
2023-10-07 22:23:15 +0000

    updated

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -40,4 +40,4 @@ class test_User(test_basemodel):
         new = self.value()
         self.assertEqual(type(new.password), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+                         type(None))
