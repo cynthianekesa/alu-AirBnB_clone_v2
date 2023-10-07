03bb60082665798fcd5de3eb879db6980197746f
Magotgarang <m.guut@alustudent.com>
2023-10-07 19:49:32 +0000

    updated

--- models/user.py
+++ models/user.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" User Module for HBNB project """
-
-=======
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
->>>>>>> parent of 06df725 (models)
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from models import storage_type
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-<<<<<<< HEAD
-    """ User class """
-    __tablename__ = 'users'
-
-=======
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
->>>>>>> parent of 06df725 (models)
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-<<<<<<< HEAD
-    reviews = relationship("Review", backref="user", cascade="all, delete")
-=======
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
->>>>>>> parent of 06df725 (models)
+    """This class defines a user by various attributes"""
+    _tablename_ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
