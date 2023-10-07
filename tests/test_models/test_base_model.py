46e65c086d35d053031303b204e38c6376b9b736
Magotgarang <m.guut@alustudent.com>
2023-10-07 22:13:35 +0000

    updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -170,4 +170,4 @@ class test_basemodel(unittest.TestCase):
         self.assertEqual(type(new.updated_at), datetime)
         n = new.to_dict()
         new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+        self.assertFalse(new.created_at == new.updated_at)
