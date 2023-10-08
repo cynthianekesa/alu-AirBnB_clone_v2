b7c0a16695beb17205b285b66a7a945093d94cf2
Magotgarang <m.guut@alustudent.com>
2023-10-08 12:16:34 +0000

    updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Amenity Module for HBNB project """
-
-=======
-"""This is the amenity class"""
->>>>>>> parent of 06df725 (models)
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from models import storage_type
 from sqlalchemy import Column, String
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-<<<<<<< HEAD
-    """ Amenity class """
+    '''amenity class'''
     __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary='place_amenity', viewonly=False)
-=======
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
->>>>>>> parent of 06df725 (models)
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
